097a408d92dda46c3d2c50d17728419f673ff68e
Christoph Behle <behlec@snakepit.(none)>
2009-04-25 19:33:07 +0200

    nop

--- FileReader.java
+++ FileReader.java
@@ -2,14 +2,30 @@ import java.io.InputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.File;
-import java.io.BufferedInputStream;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.OutputStream;
+import java.io.FileOutputStream;
 
-class FindIntput {
-	static InputStream getInput(String[] args) throws IOException {
+class FileReader {
+	static FileInputStream getInput(String[] args) throws IOException {
 		if (args.length==0) {
-			BufferedInputStream buff = new BufferedInputStream(System.in);
-			buff.mark(10000000);
-			return buff;
+			File temp = File.createTempFile("inputbuffer", ".buff");
+			temp.deleteOnExit();
+			FileOutputStream out = new FileOutputStream(temp);
+			int len;
+			int buffsize=1024;
+			byte[] buff = new byte[buffsize];
+			len = System.in.read(buff,0,buffsize);
+			while (len==buffsize) {
+				out.write(buff,0,buffsize);
+				len = System.in.read(buff,0,buffsize);
+			}
+			if (!(len==-1)) {
+				out.write(buff,0,buffsize);
+			}
+			out.close();
+			return new FileInputStream(temp);
 		} else {
 			File f=new File(args[0]);
 			if (!(f.exists()&&f.canRead())) {
--- SimpleAnalyse.java
+++ SimpleAnalyse.java
@@ -1,23 +1,53 @@
 import java.lang.Byte;
 import java.lang.Long;
 import java.io.InputStream;
-import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.HashSet;
+import java.io.IOException;
 
 
-
-class SimpleAnalyse {
-	private boolean[] bytes;
-	private HashMap<Byte,Long> frequencies;
-	private InpustStream inputStream;
-	SimpleAnalyse(InputStream in){
-		bytes = new boolean[Byte.MAX_VALUE];
-		frequencies = new HashMap<Byte,Long>();
+class SimpleAnalysis {
+	private long[] bytecount;
+	private InputStream inputStream;
+	SimpleAnalysis(InputStream in) throws IOException {
 		inputStream = in;
+		bytecount = new long[Byte.MAX_VALUE];
 		perform();
 	}
 
-	private void perform() {
-		byte b;
-		
+	private void perform() throws IOException {
+		Byte b;
+		int c;
+
+		bytecount = new long[Byte.MAX_VALUE];
+
+		try {
+			c=inputStream.read();
+			while (c!=-1) {
+				bytecount[c]=bytecount[c]+1;
+				c=inputStream.read();
+			}
+		} finally {
+			inputStream.reset();
+		}
 	}
+
+	HashSet<Byte> getBytes() {
+		HashSet<Byte> ret = new HashSet<Byte>();
+		for (byte i=0; i < Byte.MAX_VALUE; ++i) {
+			if (bytecount[i]>0) {
+				ret.add(i);
+			}
+		}
+		return ret;
+	}
+
+	Hashtable<Byte, Long> getByteCount() {
+		Hashtable<Byte,Long> ret = new Hashtable<Byte,Long>();
+		for (byte i=0; i < Byte.MAX_VALUE; ++i) {
+			ret.put(i,bytecount[i]);
+		}
+		return ret;
+	}
+
 }
