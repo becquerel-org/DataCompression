7be22e02881b29198651d71ac86145a0f2631df9
Christoph Behle <behlec@snakepit.(none)>
2009-04-26 15:13:12 +0200

    nop

---
+++ TestAnalysis.java
@@ -0,0 +1,13 @@
+import java.io.File;
+
+class TestAnalysis {
+	public static void main(String[] args) {
+		try {
+			File f = FileReader.getFile(args);
+			SimpleAnalysis count=new SimpleAnalysis(f);
+			System.out.println(count.toString());
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+	}
+}
