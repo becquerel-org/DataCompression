3ce5dc0841b52ef7c05a71e3800978be7a651c05
Christoph Behle <behlec@informatik.uni-tuebingen.de>
2009-06-16 15:19:45 +0200

    Wrote Tester.

--- tasks/BWImageReader.java
+++ tasks/BWImageReader.java
@@ -2,4 +2,6 @@ package DataCompression.tasks;
 
 public interface BWImageReader {
 	public byte getPixel(int i, int j);
+	public int getWidth();
+	public int getHeight();
 }
--- tasks/BWImageTester.java
+++ tasks/BWImageTester.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 
 public class BWImageTester {
 	
-	private PNGReader original;
+	private BWImageReader original;
 	private BWImageReader compressed;
 
 	public BWImageTester(String ori, BWImageReader comp) throws IOException {
@@ -29,6 +29,26 @@ public class BWImageTester {
 		} else {
 			return false;
 		}
+	}
+
+	public boolean compare() {
+		if (original.getHeight()!=compressed.getHeight() ||
+  			original.getWidth()!=compressed.getWidth()) {
+				System.err.println("Size or Width do not match!");
+				return false;
+		}
+		int width=original.getWidth();
+		int height=original.getHeight();
+		boolean ret=true;
+		for (int i=0; i<width;++i) {
+			for (int j=0; j<height;++j) {
+				if (!comparePixel(i,j)) {
+					System.err.println("Difference at (" + i + ", " + j+")");
+					ret=false;
+  			}
+			}
+		}
+		return ret;
 	}
 
 }
