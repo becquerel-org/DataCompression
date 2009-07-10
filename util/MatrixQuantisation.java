525a67d45025ed512b9658c5e9d50f3df92634e8
Christoph Behle <behlec@informatik.uni-tuebingen.de>
2009-07-10 08:33:31 +0200

    Quantisierung

--- util/MatrixQuantisation.java
+++ util/MatrixQuantisation.java
@@ -4,11 +4,31 @@ import DataCompression.util.DoubleMatrix;
 
 public class MatrixQuantisation {
 
-	private int doubleToInt(double x) {
-		return (int)Math.round((x*100));
+	static private int doubleToInt(double x) {
+		return (int)((x*100));
 	}
 
-	static public int[][] pack(DoubleMatrix data, DoubleMatrix quant) {
-		return new int[data.rows()][data.columns()];
+	static public int[][] pack(DoubleMatrix data, int[][] quant) {
+		int[][] ret =  new int[data.rows()][data.columns()];
+		
+		for (int i=0; i<data.rows(); ++i) {
+			for (int j=0; j<data.columns(); ++j) {
+				ret[i][j]= MatrixQuantisation.doubleToInt(
+						(int)(data.at(i,j)/quant[i][j]+0.5)
+						);
+			}
+		}
+		return ret;
 	}
+
+	static public DoubleMatrix unpack(int[][] data, int[][] quant) {
+		DoubleMatrix ret = new DoubleMatrix(data.length,data[0].length);
+
+		for (int i=0; i<data.length; ++i) {
+			for (int j=0; j<data[0].length; ++j) {
+				ret.set(i,j,data[i][j]*quant[i][j]);
+			}
+		}
+		return ret;
+	}	
 }
